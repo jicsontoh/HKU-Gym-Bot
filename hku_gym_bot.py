44c76a49a29912f4411556c9031b02da1105e4a6
jicsontoh <jicsontoh@gmail.com>
2023-06-04 23:25:06 +0800

    Update form submission

--- hku_gym_bot.py
+++ hku_gym_bot.py
@@ -1,5 +1,9 @@
+import os
+import re
+import sys
+import getopt
 from telnetlib import EC
-
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -9,86 +13,130 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
+load_dotenv()
 
-ser = Service("/Applications/Google\ Chrome.app")
-# ser = Service(r"/Applications/Google\ Chrome.app")
+EMAIL = os.getenv("EMAIL")
+NAME = os.getenv("NAME")
+Student_no = os.getenv("STUDENT_NUM")
 
-op = webdriver.ChromeOptions()
-# op.add_argument('--headless')
-# op.add_argument("start-maximized")
-op.add_argument("--incognito")
-op.add_argument("user-data-dir=selenium")
-op.add_experimental_option("excludeSwitches", ["enable-automation"])
-op.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(service=ser, options=op)
+SER = Service("/Applications/Google\ Chrome.app")
+LINK = "https://fcbooking.cse.hku.hk/Form/SignUp"
 
-EMAIL = "u3611050@connect.hku.hk"
-NAME = "Toh Ji Xiang Jicson"
-Student_no = "303611050"
+SESSIONS = {"CSE Active": {"1245-1415": "10123", "1715-1845": "10124"},
+            "HKU B-Active": {"1700-1830": "10125", "1845-2015": "10126", "2030-2200": "10127"},
+            "Stanley Ho Sports Centre": {"1745-1915": "10128", "1930-2100": "10129"}}
+
+CENTRE = {"CSE": "CSE Active", "B-Active": "HKU B-Active", "SHSC": "Stanley Ho Sports Centre"}
 
 
-def hku_gym_bot():
-    print("Bot running...")
+def hku_gym_bot(centre_name, select_date, select_time):
+    try:
+        op = webdriver.ChromeOptions()
+        # op.add_argument('--headless')
+        # op.add_argument("start-maximized")
+        op.add_argument("--incognito")
+        op.add_argument("user-data-dir=selenium")
+        op.add_experimental_option("excludeSwitches", ["enable-automation"])
+        op.add_experimental_option('useAutomationExtension', False)
+        driver = webdriver.Chrome(service=SER, options=op)
 
-    # website to open
-    driver.get("https://fcbooking.cse.hku.hk/Form/SignUp")
-    # driver.maximize_window()
-    driver.set_window_size(893, 786)
+        print("Bot running...")
+        # Open booking website
+        driver.get(LINK)
+        driver.set_window_size(893, 789)
 
-    # // img[@class ="imageWithFallback"]
-    # link = driver.find_element(By.XPATH, "//a[normalize-space()='sign up now']")
-    # link = driver.find_element(By.XPATH, "//img[contains(@class,'largeHeader__logo')]")
-    email = driver.find_element(By.XPATH, "//input[@id='Email']")
-    email.send_keys(EMAIL)
-    print("Keyed in Email")
-    name = driver.find_element(By.XPATH, "//input[@id='FirstName']")
-    name.send_keys(NAME)
-    print("Keyed in Name")
-    student_no = driver.find_element(By.XPATH, "//input[@id='MemberID']")
-    student_no.send_keys(Student_no)
-    print("Keyed in Student No.")
-    centre = Select(driver.find_element(By.XPATH, "//select[@id='CenterID']"))
-    # centre.select_by_visible_text("HKU B-Active")
-    centre.select_by_visible_text("CSE Active")
-    print("Selected Centre")
-    date = Select(driver.find_element(By.XPATH, "//select[@id='DateList']"))
-    date.select_by_value("2023/03/15")
-    print("Selected Date")
-    time = Select(driver.find_element(By.XPATH, "//select[@id='SessionTime']"))
-    time.select_by_value("10124")
-    print("Selected Time")
-    data_collection = driver.find_element(By.XPATH, "//label[@for='dataCollection']")
-    data_collection.click()
-    print("Selected Date")
+        # Input email
+        email = driver.find_element(By.XPATH, "//input[@id='Email']")
+        email.send_keys(EMAIL)
+        print("Keyed in Email")
 
-    driver.execute_script("window.scrollTo(0, 988)")
+        # Input name
+        name = driver.find_element(By.XPATH, "//input[@id='FirstName']")
+        name.send_keys(NAME)
+        print("Keyed in Name")
 
-    sleep(10)
-    driver.implicitly_wait(10)
-    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
-        (By.CSS_SELECTOR, "iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+        # Input Student no.
+        student_no = driver.find_element(By.XPATH, "//input[@id='MemberID']")
+        student_no.send_keys(Student_no)
+        print("Keyed in Student No.")
 
-    print("Clicked Recaptcha")
-    sleep(5)
-    driver.switch_to.parent_frame()
+        # Select gym centre
+        centre = Select(driver.find_element(By.XPATH, "//select[@id='CenterID']"))
+        centre.select_by_visible_text(centre_name)
+        print(f"Selected Centre: {centre_name}")
 
-    select = driver.find_element(By.XPATH, "//button[@id='sbmtBtn']")
-    select.click()
-    print("Clicked Submit")
+        # Select Date
+        date = Select(driver.find_element(By.XPATH, "//select[@id='DateList']"))
+        # Date format: YYYY/MM/DD
+        date.select_by_value(select_date)
+        print(f"Selected Date: {select_date}")
 
-    sleep(30)
+        # Select timeslot
+        time = Select(driver.find_element(By.XPATH, "//select[@id='SessionTime']"))
+        time.select_by_value(select_time)
+        print(f"Selected Time: {select_time}")
 
-    print("Booking Success!")
+        # Click declaration
+        data_collection = driver.find_element(By.XPATH, "//label[@for='dataCollection']")
+        data_collection.click()
+        print("Clicked declaration")
 
-    # except Exception as e:
-    #     print(e)
-    #     return False
-    #
-    # finally:
-    #     driver.quit()
+        # Bypass reCAPTCHA
+        driver.execute_script("window.scrollTo(0, 988)")
+        sleep(10)
+        driver.implicitly_wait(10)
+        WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
+            (By.CSS_SELECTOR, "iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+
+        print("Clicked Recaptcha")
+        sleep(5)
+        driver.switch_to.parent_frame()
+
+        # Click submit
+        select = driver.find_element(By.XPATH, "//button[@id='sbmtBtn']")
+        select.click()
+        print("Clicked Submit")
+
+        sleep(30)
+        print("Booking Success!")
+
+    except Exception as e:
+        print(f"[Bot error] {e}.")
+
+
+def main(argv):
+    try:
+        centre = argv[0]
+        date = argv[1]
+        timeslot = argv[2]
+
+        check_centre(centre)
+        check_date(date)
+        check_time(centre, timeslot)
+
+        hku_gym_bot(CENTRE[centre], date, SESSIONS[CENTRE[centre]][timeslot])
+    except Exception as e:
+        print(f"[Input Error] {e}.")
+
+
+def check_centre(centre):
+    if centre not in CENTRE.keys():
+        raise Exception("No such centre")
+
+
+def check_date(date):
+    # Input date format YYYY/MM/DD
+    pattern = r'^\d{4}/\d{2}/\d{2}$'
+    if not re.match(pattern, date):
+        raise Exception("Wrong date format")
+
+
+def check_time(centre, timeslot):
+    if timeslot not in SESSIONS[CENTRE[centre]].keys():
+        raise Exception("Timeslot not available in the selected centre")
 
 
 if __name__ == "__main__":
-    hku_gym_bot()
+    main(sys.argv[1:])
